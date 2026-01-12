030b130df98d5f5b9737a027498632c6be579912
itsivag <sivacbrf2@gmail.com>
2026-01-12 12:36:32 +0530

    delete: unwanted targets

--- library/src/jvmMain/kotlin/MixPanelAnalyticsTracker.jvm.kt
+++
@@ -1,9 +0,0 @@
-package com.suprbeta.kuri.analytics
-
-actual class MixPanelAnalyticsTracker : AnalyticsTracker {
-    actual override fun trackEvent(
-        event: String,
-        properties: Map<String, Any>?
-    ) {
-    }
-}
--- library/src/linuxMain/kotlin/MixPanelAnalyticsTracker.linux.kt
+++
@@ -1,9 +0,0 @@
-package com.suprbeta.kuri.analytics
-
-actual class MixPanelAnalyticsTracker : com.suprbeta.kuri.analytics.AnalyticsTracker {
-    actual override fun trackEvent(
-        event: String,
-        properties: Map<String, Any>?
-    ) {
-    }
-}
